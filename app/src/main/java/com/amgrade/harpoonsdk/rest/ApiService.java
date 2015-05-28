d410aea1c625cb1a8b878abced161ed5bb29d940
Michael Dontsov <crocotiger@gmail.com>
2015-05-28 17:54:48 +0300

    fixes by feedback

--- app/src/main/java/com/amgrade/harpoonsdk/rest/ApiService.java
+++ app/src/main/java/com/amgrade/harpoonsdk/rest/ApiService.java
@@ -19,37 +19,37 @@ public interface ApiService {
     //-------------------------------------------------------------------
     //Brand api methods
     //-------------------------------------------------------------------
-    @GET("/{version}/{user_id}/brand/{format}")
+    @GET("/v1/{user_id}/brand/{format}")
     void getAvailableBrands(@Header("Content-Type")String type, @Header("appid")String appId,
                             @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                            @Path("version")String version, @Path("user_id")Integer id, @Path("format")String format,
+                            /*@Path("version")String version, */@Path("user_id")Integer id, @Path("format")String format,
                             @Query("token")String token, @Body ListParams params);
 
-    @GET("/{version}/{user_id}/brand/{brand_id}/{format}")
+    @GET("/v1/{user_id}/brand/{brand_id}/{format}")
     void getBrandDetails(@Header("Content-Type")String type, @Header("appid")String appId,
                          @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                         @Path("version")String version, @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                         /*@Path("version")String version, */@Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
                          @Path("format")String format, @Query("token")String token);
 
     //-------------------------------------------------------------------
     //Campaign api methods
     //-------------------------------------------------------------------
-    @POST("/{version}/{user_id}/brand/{brand_id}/campaign/{format}")
+    @POST("/v1/{user_id}/brand/{brand_id}/campaign/{format}")
     void createCampaign(@Header("Content-Type")String type, @Header("appid")String appId,
                         @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                        @Path("version")String version, @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                        /*@Path("version")String version, */@Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
                          @Path("format")String format, @Query("token")String token);
 
-    @GET("/{version}/{user_id}/brand/{brand_id}/campaign/{format}")
+    @GET("/v1/{user_id}/brand/{brand_id}/campaign/{format}")
     void getCampaigns(@Header("Content-Type")String type, @Header("appid")String appId,
                       @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                      @Path("version")String version, @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                      /*@Path("version")String version, */@Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
                       @Path("format")String format, @Query("token")String token);
 
-    @GET("/{version}/{user_id}/brand/{brand_id}/campaign/{campaign_id}/{format}")
+    @GET("/v1/{user_id}/brand/{brand_id}/campaign/{campaign_id}/{format}")
     void updateCampaign(@Header("Content-Type")String type, @Header("appid")String appId,
                         @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                        @Path("version")String version, @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                        /*@Path("version")String version, */@Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
                         @Path("campaign_id")Integer campaign_id, @Path("format")String format, @Query("token")String token);
 
     //-------------------------------------------------------------------

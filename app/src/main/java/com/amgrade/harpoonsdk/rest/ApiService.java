544b700cd97ad4f700ac91af8ea10917012664d2
Michael Dontsov <crocotiger@gmail.com>
2015-05-29 19:33:16 +0300

    events, started models

--- app/src/main/java/com/amgrade/harpoonsdk/rest/ApiService.java
+++ app/src/main/java/com/amgrade/harpoonsdk/rest/ApiService.java
@@ -16,19 +16,22 @@ public interface ApiService {
 
     //TODO add "Callback" params, implement response classes for methods!!!!!!!!!!!!!!!!
 
+    //You can use "{version}" in path (instead of "v1") and "@Path("version")String version" in method params
+    //if you want to let user choose an API version to work with
+
     //-------------------------------------------------------------------
     //Brand api methods
     //-------------------------------------------------------------------
     @GET("/v1/{user_id}/brand/{format}")
     void getAvailableBrands(@Header("Content-Type")String type, @Header("appid")String appId,
                             @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                            /*@Path("version")String version, */@Path("user_id")Integer id, @Path("format")String format,
+                            @Path("user_id")Integer id, @Path("format")String format,
                             @Query("token")String token, @Body ListParams params);
 
     @GET("/v1/{user_id}/brand/{brand_id}/{format}")
     void getBrandDetails(@Header("Content-Type")String type, @Header("appid")String appId,
                          @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                         /*@Path("version")String version, */@Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                         @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
                          @Path("format")String format, @Query("token")String token);
 
     //-------------------------------------------------------------------
@@ -37,24 +40,38 @@ public interface ApiService {
     @POST("/v1/{user_id}/brand/{brand_id}/campaign/{format}")
     void createCampaign(@Header("Content-Type")String type, @Header("appid")String appId,
                         @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                        /*@Path("version")String version, */@Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
-                         @Path("format")String format, @Query("token")String token);
+                        @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                        @Path("format")String format, @Query("token")String token);
 
     @GET("/v1/{user_id}/brand/{brand_id}/campaign/{format}")
     void getCampaigns(@Header("Content-Type")String type, @Header("appid")String appId,
                       @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                      /*@Path("version")String version, */@Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                      @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
                       @Path("format")String format, @Query("token")String token);
 
     @GET("/v1/{user_id}/brand/{brand_id}/campaign/{campaign_id}/{format}")
     void updateCampaign(@Header("Content-Type")String type, @Header("appid")String appId,
                         @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
-                        /*@Path("version")String version, */@Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                        @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
                         @Path("campaign_id")Integer campaign_id, @Path("format")String format, @Query("token")String token);
 
     //-------------------------------------------------------------------
     //Event api methods
     //-------------------------------------------------------------------
+    @GET("/v1/{user_id}/brand/{brand_id}/event/{event_id}/{format}")
+    void getEventDetails(@Header("Content-Type")String type, @Header("appid")String appId,
+                         @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
+                         @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                         @Path("event_id")Integer event_id, @Path("event_id_type")String event_type_id,
+                         @Path("format")String format, @Query("token")String token);
 
+    @GET("/v1/{user_id}/brand/{brand_id}/event/{format}")
+    void getEventList(@Header("Content-Type")String type, @Header("appid")String appId,
+                      @Header("appsecret")String appSecret, @Header("appbundle")String appBundle,
+                      @Path("user_id")Integer user_id, @Path("brand_id")Integer brand_id,
+                      @Path("format")String format, @Query("token")String token,
+                      @Body("filter"));
+
+//    @GET("")
 
 }
